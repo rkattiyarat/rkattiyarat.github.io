3d3ea5e61a3c4e14baf1f77b4b3f6c92bd755210
rose <aom_bnn@hotmail.com>
2026-04-25 16:44:29 -0600

    add base url

--- weather-app.js
+++ weather-app.js
@@ -1,5 +1,5 @@
 //const API_BASE_URL = "http://localhost:3000";
-const API_BASE_URL = "https://your-app.onrender.com";
+const API_BASE_URL = "https://weather-backend-fj3b.onrender.com/";
 
 async function loadCityName(cityName) {
     let response = await fetch(`${API_BASE_URL}/weather?city=${cityName}`);
